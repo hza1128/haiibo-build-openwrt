45697ee495700ea0c7d1e418e16c0c75283585dc
hza1128 <141570215+hza1128@users.noreply.github.com>
2024-10-13 20:16:45 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+#!/bin/bash
+
+# 添加软件源
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
